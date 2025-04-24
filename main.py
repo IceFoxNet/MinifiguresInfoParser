bc2678f2a1a385a27417cc17d485ae22abfa2025
IceFoxNet <icefoxnet@gmail.com>
2025-04-24 18:33:13 +0300

    Global Async Added

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 
 while True:
     try:
-        from playwright.sync_api import sync_playwright, expect
-        from configparser import ConfigParser
-        import gspread, pathlib, requests
+        from playwright.async_api import async_playwright, Browser
+        import gspread, pathlib, requests, asyncio
+        from async_lru import alru_cache
     except ImportError as e:
         package = e.msg.split()[-1][1:-1]
         os.system(f'python -m pip install {package}')
@@ -12,21 +12,15 @@ while True:
         break
 
 dir = pathlib.Path(__file__).parent.resolve()
+rub = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()['Valute']['USD']['Value']
 
-config = ConfigParser()
-config.read(os.path.join(dir, 'config.ini'))
-sheet_url = config.get('parser', 'url')
-
-def main(start: int, end: int, setup: dict):
+async def main(start: int, end: int, setup: dict):
     
     print('Подготавливаем всё для работы')
     if start < 3: start = 3
-    cache = {}
 
     # ==> ПОДКЛЮЧЕНИЕ ГУГЛ-АККАУНТА
-    creds = setup.get('GoogleCredentials')
-    google_client = gspread.authorize(creds)
-    spreadsheet = google_client.open_by_url(sheet_url)
+    spreadsheet: gspread.spreadsheet.Spreadsheet = setup.get('MinifiguresSheet')
     sheet = spreadsheet.worksheet('Минифигурки')
     
     # ==> ПОЛУЧЕНИЕ ДАННЫХ С ТАБЛИЦЫ
@@ -36,65 +30,24 @@ def main(start: int, end: int, setup: dict):
     name_res = []
     series_res = []
 
-    # ==> ПОЛУЧЕНИЕ КУРСА ДОЛЛАРА
-    rub = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()['Valute']['USD']['Value']
-
     print('Начинаем работу с браузером')
 
     # ==> РАБОТА С БРАУЗЕРОМ
-    for idx in range(len(articles)):
-        try: 
-            if not articles[idx].value: continue
-        except: 
-            break
-        art = articles[idx].value
-        if art in cache:
-            qty_res.append([cache[art][0]])
-            price_res.append([cache[art][1]])
-            name_res.append([cache[art][2]])
-            series_res.append([cache[art][3]])
-            continue
-        with sync_playwright() as p:
-            driver = p.chromium.launch(proxy={
-                'server': 'http://166.0.211.142:7576',
-                'username': 'user258866',
-                'password': 'pe9qf7'
-            })
-            page = driver.new_page()
-            try:
-                page.goto(f'https://www.bricklink.com/v2/catalog/catalogitem.page?M={articles[idx].value}#T=P')
-                page.wait_for_selector('table.pcipgMainTable')
-                table = page.query_selector('#_idPGContents > table > tbody > tr:nth-child(3) > td:nth-child(4)')
-                rows = table.query_selector_all('tr')
-                qty_val = int(rows[1].query_selector_all('td')[-1].text_content())
-                prc_val = round(float(rows[4].query_selector_all('td')[-1].text_content()[4:]) * rub)
-                name_val = page.query_selector('#item-name-title').text_content()
-                catalog_line = page.query_selector('#content > div > table > tbody > tr > td:nth-child(1)').query_selector_all('a')
-                series_val = catalog_line[2].inner_text()
-                if series_val == 'Super Heroes':
-                    series_val = page.query_selector('#id_divBlock_Main > table:nth-child(1) > tbody > tr:nth-child(3) > td > div:nth-child(1) > table > tbody > tr > td > a').inner_text().split()[0]
-                elif series_val == 'Town':
-                    series_val = 'City'
-                elif series_val == 'Space':
-                    try:
-                        if catalog_line[3].inner_text() == 'Galaxy Squad':
-                            series_val = 'Galaxy Squad'
-                    except:
-                        series_val = 'Space'
-            except Exception as e:
-                print(f'Ошибка при работе с артикулом {articles[idx].value} (https://www.bricklink.com/v2/catalog/catalogitem.page?M={articles[idx].value}#T=P')
-                qty_res.append([None])
-                price_res.append([None])
-                name_res.append([None])
-                series_res.append([None])
-            else:
-                print(f'Обработка артикула {articles[idx].value}: {series_val}, {name_val}, {prc_val}, {qty_val}')
-                qty_res.append([qty_val])
-                price_res.append([prc_val])
-                name_res.append([name_val])
-                series_res.append([series_val])
-                cache[art] = (qty_val, prc_val, name_val, series_val)
-            finally: continue
+
+    semaphore = asyncio.Semaphore(4)
+    async with async_playwright() as p: 
+        browser = await p.chromium.launch(proxy={
+            'server': 'http://166.0.211.142:7576',
+            'username': 'user258866',
+            'password': 'pe9qf7'
+        })
+        tasks = [parse_item(semaphore, browser, articles[idx].value) for idx in range(len(articles))]
+        results = await asyncio.gather(*tasks)
+    for res in results:
+        qty_res.append([res[0]])
+        price_res.append([res[1]])
+        name_res.append([res[2]])
+        series_res.append([res[3]])
     print(f'Загружаем информацию на таблицу')
     sheet.update(qty_res, f'V{start}:V{len(qty_res)+start}')
     sheet.update(price_res, f'T{start}:T{len(price_res)+start}')
@@ -102,6 +55,49 @@ def main(start: int, end: int, setup: dict):
     sheet.update(series_res, f'A{start}:A{len(series_res)+start}')
     print(f'Программа завершила выполнение')
 
+@alru_cache(None)
+async def parse_item(semaphore: asyncio.Semaphore, driver: Browser, art: str):
+    global rub
+    async with semaphore:
+        print(f'Обработка артикула {art}')
+        page = await driver.new_page()
+        try:
+            await page.goto(f'https://www.bricklink.com/v2/catalog/catalogitem.page?M={art}#T=P')
+            await page.wait_for_selector('table.pcipgMainTable')
+            table = await page.query_selector('#_idPGContents > table > tbody > tr:nth-child(3) > td:nth-child(4)')
+            rows = await table.query_selector_all('tr')
+            # Get Qty Value
+            rows_1_all_td = await rows[1].query_selector_all('td')
+            qty_val = int(await rows_1_all_td[-1].text_content())
+            # Get Price Value
+            rows_4_all_td = await rows[4].query_selector_all('td')
+            rows_4_text_content = await rows_4_all_td[-1].text_content()
+            prc_val = round(float(rows_4_text_content[4:]) * rub)
+            # Get Name Value
+            item_name_title_div = await page.query_selector('#item-name-title')
+            name_val = await item_name_title_div.text_content()
+            # Get Series Value
+            catalog_line = await (await page.query_selector('#content > div > table > tbody > tr > td:nth-child(1)')).query_selector_all('a')
+            series_val = await catalog_line[2].inner_text()
+            if series_val == 'Super Heroes':
+                co_autor = await page.query_selector('#id_divBlock_Main > table:nth-child(1) > tbody > tr:nth-child(3) > td > div:nth-child(1) > table > tbody > tr > td > a')
+                co_autor_text = await co_autor.inner_text()
+                series_val = co_autor_text.split()[0]
+            elif series_val == 'Town':
+                series_val = 'City'
+            elif series_val == 'Space':
+                try:
+                    if await catalog_line[3].inner_text() == 'Galaxy Squad':
+                        series_val = 'Galaxy Squad'
+                except:
+                    series_val = 'Space'
+        except Exception as e:
+            await page.close()
+            return((None, None, None, None))
+        else:
+            await page.close()
+            return((qty_val, prc_val, name_val, series_val))
+
 if __name__ == '__main__':
     from Setup.setup import setup
-    main(3, 1_000, setup)
+    asyncio.run(main(3, 10, setup))
